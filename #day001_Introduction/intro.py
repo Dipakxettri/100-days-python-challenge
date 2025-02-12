8dd4033aad866eff4a006921202ef32e3affc0e9
deepak <codeepakghimire@gmail.com>
2025-02-12 19:00:38 +0545

    Updated

---
+++ #day001_Introduction/intro.py
@@ -0,0 +1,12 @@
+# hello world
+print("hello world")  
+
+# high level object oriented programming language 
+# relesed in 1989
+# created by Guido Van Rossum
+# easy to learn
+
+# platform independend
+# open source
+# powerfull in AI/ML
+# Huge Community
